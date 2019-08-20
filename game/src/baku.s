fdf102116453c9be05ff8074c25dcc2f4dd80b50
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2019-08-20 08:52:01 +0200

    clean up aob parts

---
+++ game/src/baku.s
@@ -0,0 +1,233 @@
+    .global Aob_Panel_Baku_00
+Aob_Panel_Baku_00:
+    .2byte 4
+    .2byte 0x0006, 0x31F3, 0x8602
+    .2byte 0x00F2, 0x0005, 0x8602
+    .2byte 0x00F2, 0x11F3, 0x8602
+    .2byte 0x0006, 0x2005, 0x8602
+
+    .align 2
+    .global Aob_Panel_Baku_01
+Aob_Panel_Baku_01:
+    .2byte 4
+    .2byte 0x0007, 0x31F2, 0x8602
+    .2byte 0x00F1, 0x0006, 0x8602
+    .2byte 0x00F1, 0x11F2, 0x8602
+    .2byte 0x0007, 0x2006, 0x8602
+
+    .align 2
+    .global Aob_Panel_Baku_02
+Aob_Panel_Baku_02:
+    .2byte 4
+    .2byte 0x0008, 0x31F1, 0x8602
+    .2byte 0x00F0, 0x0007, 0x8602
+    .2byte 0x00F0, 0x11F1, 0x8602
+    .2byte 0x0008, 0x2007, 0x8602
+
+    .align 2
+    .global Aob_Panel_Baku_03
+Aob_Panel_Baku_03:
+    .2byte 4
+    .2byte 0x0009, 0x31F0, 0x8602
+    .2byte 0x00EF, 0x0008, 0x8602
+    .2byte 0x00EF, 0x11F0, 0x8602
+    .2byte 0x0009, 0x2008, 0x8602
+
+    .align 2
+    .global Aob_Panel_Baku_04
+Aob_Panel_Baku_04:
+    .2byte 4
+    .2byte 0x00EF, 0x01EF, 0x8603
+    .2byte 0x00EF, 0x0009, 0x8603
+    .2byte 0x0009, 0x01EF, 0x8603
+    .2byte 0x0009, 0x0009, 0x8603
+
+    .align 2
+    .global Aob_Panel_Baku_05
+Aob_Panel_Baku_05:
+    .2byte 4
+    .2byte 0x00EE, 0x01EE, 0x8603
+    .2byte 0x00EE, 0x000A, 0x8603
+    .2byte 0x000A, 0x01EE, 0x8603
+    .2byte 0x000A, 0x000A, 0x8603
+
+    .align 2
+    .global Aob_Panel_Baku_06
+Aob_Panel_Baku_06:
+    .2byte 4
+    .2byte 0x00ED, 0x01ED, 0x8603
+    .2byte 0x00ED, 0x000B, 0x8603
+    .2byte 0x000B, 0x01ED, 0x8603
+    .2byte 0x000B, 0x000B, 0x8603
+
+    .align 2
+    .global Aob_Panel_Baku_07
+Aob_Panel_Baku_07:
+    .2byte 4
+    .2byte 0x00EC, 0x01EC, 0x8603
+    .2byte 0x00EC, 0x000C, 0x8603
+    .2byte 0x000C, 0x01EC, 0x8603
+    .2byte 0x000C, 0x000C, 0x8603
+
+    .align 2
+    .global Aob_Panel_Baku_08
+Aob_Panel_Baku_08:
+    .2byte 4
+    .2byte 0x00EB, 0x01EB, 0x8604
+    .2byte 0x00EB, 0x000D, 0x8604
+    .2byte 0x000D, 0x01EB, 0x8604
+    .2byte 0x000D, 0x000D, 0x8604
+
+    .align 2
+    .global Aob_Panel_Baku_09
+Aob_Panel_Baku_09:
+    .2byte 4
+    .2byte 0x00EA, 0x01EA, 0x8604
+    .2byte 0x00EA, 0x000E, 0x8604
+    .2byte 0x000E, 0x01EA, 0x8604
+    .2byte 0x000E, 0x000E, 0x8604
+
+    .align 2
+    .global Aob_Panel_Baku_0a
+Aob_Panel_Baku_0a:
+    baserom 0xB10, 0x1C
+
+    .global Aob_Panel_Baku_0b
+Aob_Panel_Baku_0b:
+    baserom 0xB2C, 0x1C
+
+    .global Aob_Panel_Baku_Ani00
+Aob_Panel_Baku_Ani00:
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte 0
+
+    .global Aob_Panel_Baku_Ani01
+Aob_Panel_Baku_Ani01:
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte 0
+
+    .global Aob_Panel_Baku_Ani02
+Aob_Panel_Baku_Ani02:
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte 0
+
+    .global Aob_Panel_Baku_Ani03
+Aob_Panel_Baku_Ani03:
+    .4byte Aob_Panel_Baku_00
+    .4byte Aob_Panel_Baku_01
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_02
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_03
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_04
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_05
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_06
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_07
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_08
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte Aob_Panel_Baku_09
+    .4byte 0
---
+++ game/src/pupuri.s
@@ -0,0 +1,658 @@
+    .global Aob_Pupuri_Parts_00
+Aob_Pupuri_Parts_00:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE70D
+    .2byte 0x40FF, 0x0016, 0xE70F
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x00E8, 0x41FA, 0xE715
+    .2byte 0x80E8, 0x000A, 0xE719
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+
+    .align 2
+    .global Aob_Pupuri_Parts_01
+Aob_Pupuri_Parts_01:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE723
+    .2byte 0x40FF, 0x0016, 0xE725
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x00E8, 0x41FA, 0xE715
+    .2byte 0x80E8, 0x000A, 0xE719
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+
+    .align 2
+    .global Aob_Pupuri_Parts_02
+Aob_Pupuri_Parts_02:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE727
+    .2byte 0x40FF, 0x0016, 0xE729
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x00E8, 0x41FA, 0xE715
+    .2byte 0x80E8, 0x000A, 0xE719
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+
+    .align 2
+    .global Aob_Pupuri_Parts_03
+Aob_Pupuri_Parts_03:
+    .2byte 16
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x0004, 0x0004, 0xE72B
+    .2byte 0x40FF, 0x0016, 0xE72C
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x0018, 0x41F0, 0xE72E
+    .2byte 0x0018, 0x4018, 0xE732
+    .2byte 0x00E9, 0x01FB, 0xE736
+    .2byte 0x80EA, 0x000A, 0xE737
+    .2byte 0x00E1, 0x0003, 0xE739
+    .2byte 0x00E9, 0x0003, 0xE73A
+
+    .align 2
+    .global Aob_Pupuri_Parts_04
+Aob_Pupuri_Parts_04:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x00F2, 0x000A, 0xE74F
+    .2byte 0x00E2, 0x4002, 0xE750
+
+    .align 2
+    .global Aob_Pupuri_Parts_05
+Aob_Pupuri_Parts_05:
+    .2byte 15
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x80EA, 0x000A, 0xE754
+    .2byte 0x00EA, 0x0002, 0xE756
+    .2byte 0x00DA, 0x4004, 0xE757
+
+    .global Aob_Pupuri_Parts_06
+Aob_Pupuri_Parts_06:
+    .2byte 15
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x00EF, 0x4012, 0xE75B
+    .2byte 0x00F4, 0x41F0, 0xE75F
+    .2byte 0x80F4, 0x0000, 0xE763
+    .2byte 0x4004, 0x01F0, 0xE765
+    .2byte 0x0004, 0x0000, 0xE767
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x80EA, 0x000A, 0xE754
+    .2byte 0x00EA, 0x0002, 0xE756
+    .2byte 0x00DA, 0x4004, 0xE757
+
+    .global Aob_Pupuri_Parts_07
+Aob_Pupuri_Parts_07:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE70D
+    .2byte 0x40FF, 0x0016, 0xE70F
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x00E8, 0x41FA, 0xE768
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+    .2byte 0x80E8, 0x000A, 0xE719
+
+    .align 2
+    .global Aob_Pupuri_Parts_08
+Aob_Pupuri_Parts_08:
+    .2byte 15
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE727
+    .2byte 0x40FF, 0x0016, 0xE729
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x00EA, 0x41FD, 0xE76C
+    .2byte 0x80EA, 0x000D, 0xE770
+    .2byte 0x0018, 0x41F0, 0xE772
+    .2byte 0x8018, 0x0018, 0xE776
+    .2byte 0x001F, 0x0020, 0xE778
+
+    .global Aob_Pupuri_Parts_09
+Aob_Pupuri_Parts_09:
+    .2byte 15
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x80EA, 0x0009, 0xE754
+    .2byte 0x00EA, 0x0001, 0xE756
+    .2byte 0x00DA, 0x4003, 0xE757
+
+    .global Aob_Pupuri_Parts_0a
+Aob_Pupuri_Parts_0a:
+    .2byte 15
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0018, 0x4018, 0xE732
+    .2byte 0x0018, 0x41F0, 0xE72E
+    .2byte 0x80EA, 0x0009, 0xE754
+    .2byte 0x00EA, 0x0001, 0xE756
+    .2byte 0x00DA, 0x4003, 0xE757
+
+    .global Aob_Pupuri_Parts_0b
+Aob_Pupuri_Parts_0b:
+    baserom 0x1120, 0x5C
+
+    .global Aob_Pupuri_Parts_0c
+Aob_Pupuri_Parts_0c:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x0004, 0x0004, 0xE72B
+    .2byte 0x40FF, 0x0016, 0xE72C
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x0018, 0x41F0, 0xE72E
+    .2byte 0x0018, 0x4018, 0xE732
+    .2byte 0x00E8, 0x41FA, 0xE768
+    .2byte 0x80E8, 0x000A, 0xE719
+
+    .align 2
+    .global Aob_Pupuri_Parts_0d
+Aob_Pupuri_Parts_0d:
+    .2byte 14
+    .2byte 0x00E8, 0x41FA, 0xE768
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x80E8, 0x000A, 0xE719
+
+    .align 2
+    .global Aob_Pupuri_Parts_0e
+Aob_Pupuri_Parts_0e:
+    .2byte 14
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x00E8, 0x41FA, 0xE715
+    .2byte 0x80E8, 0x000A, 0xE719
+
+    .align 2
+    .global Aob_Pupuri_Parts_0f
+Aob_Pupuri_Parts_0f:
+    .2byte 15
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x00FE, 0x41FE, 0xE73B
+    .2byte 0x00FD, 0x4016, 0xE73F
+    .2byte 0x4004, 0x0009, 0xE743
+    .2byte 0x000D, 0x01FF, 0xE745
+    .2byte 0x0005, 0x001E, 0xE746
+    .2byte 0x0017, 0x41EF, 0xE747
+    .2byte 0x0015, 0x4017, 0xE74B
+    .2byte 0x80EA, 0x0009, 0xE754
+    .2byte 0x00EA, 0x0001, 0xE756
+    .2byte 0x00DA, 0x4003, 0xE757
+
+    .global Aob_Pupuri_Parts_10
+Aob_Pupuri_Parts_10:
+    .2byte 15
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE70D
+    .2byte 0x40FF, 0x0016, 0xE70F
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x00E8, 0x41FA, 0xE768
+    .2byte 0x0018, 0x41F0, 0xE772
+    .2byte 0x80E8, 0x000A, 0xE719
+    .2byte 0x8018, 0x0018, 0xE776
+    .2byte 0x001F, 0x0020, 0xE778
+
+    .global Aob_Pupuri_Parts_11
+Aob_Pupuri_Parts_11:
+    .2byte 12
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE70D
+    .2byte 0x40FF, 0x0016, 0xE70F
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+
+    .align 2
+    .global Aob_Pupuri_Parts_12
+Aob_Pupuri_Parts_12:
+    .2byte 12
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE723
+    .2byte 0x40FF, 0x0016, 0xE725
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+
+    .align 2
+    .global Aob_Pupuri_Parts_13
+Aob_Pupuri_Parts_13:
+    .2byte 12
+    .2byte 0x00EE, 0x4013, 0xE700
+    .2byte 0x00F4, 0x41EC, 0xE704
+    .2byte 0x80F4, 0x01FC, 0xE708
+    .2byte 0x4004, 0x01EC, 0xE70A
+    .2byte 0x0004, 0x01FC, 0xE70C
+    .2byte 0x4005, 0x01FE, 0xE727
+    .2byte 0x40FF, 0x0016, 0xE729
+    .2byte 0x4004, 0x0009, 0xE711
+    .2byte 0x000D, 0x01FF, 0xE713
+    .2byte 0x0005, 0x001E, 0xE714
+    .2byte 0x0018, 0x41F0, 0xE71B
+    .2byte 0x0018, 0x4018, 0xE71F
+
+    .align 2
+    .global Aob_Pupuri_Parts_14
+Aob_Pupuri_Parts_14:
+    .2byte 2
+    .2byte 0x00E8, 0x41FA, 0xE715
+    .2byte 0x80E8, 0x000A, 0xE719
+
+    .align 2
+    .global Aob_Pupuri_Parts_15
+Aob_Pupuri_Parts_15:
+    .2byte 2
+    .2byte 0x00E8, 0x41FA, 0xE768
+    .2byte 0x80E8, 0x000A, 0xE719
+
+    .global Aob_Pupuri_Parts_Ani00
+Aob_Pupuri_Parts_Ani00:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_11
+    .byte 0xF8
+    .word 0x200B275
+    .byte 5
+    .word Aob_Pupuri_Parts_12
+    .byte 8
+    .word Aob_Pupuri_Parts_13
+    .byte 5
+    .word Aob_Pupuri_Parts_12
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani00
+
+    .global Aob_Pupuri_Parts_Ani01
+Aob_Pupuri_Parts_Ani01:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_14
+    .byte 0xF8
+    .word 0x200B275
+    .byte 10
+    .word Aob_Pupuri_Parts_15
+    .byte 4
+    .word Aob_Pupuri_Parts_14
+    .byte 9
+    .word Aob_Pupuri_Parts_15
+    .byte 5
+    .word Aob_Pupuri_Parts_14
+    .byte 8
+    .word Aob_Pupuri_Parts_15
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani01
+
+    .global Aob_Pupuri_Parts_Ani02
+Aob_Pupuri_Parts_Ani02:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_00
+    .byte 4
+    .word Aob_Pupuri_Parts_01
+    .byte 12
+    .word Aob_Pupuri_Parts_02
+    .byte 6
+    .word Aob_Pupuri_Parts_0c
+    .byte 6
+    .word Aob_Pupuri_Parts_0d
+    .byte 12
+    .word Aob_Pupuri_Parts_0e
+    .byte 6
+    .word Aob_Pupuri_Parts_0d
+    .byte 12
+    .word Aob_Pupuri_Parts_0e
+    .byte 4
+    .word Aob_Pupuri_Parts_01
+    .byte 12
+    .word Aob_Pupuri_Parts_02
+    .byte 0xF8
+    .word 0x200B299
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani00
+
+    .global Aob_Pupuri_Parts_Ani03
+Aob_Pupuri_Parts_Ani03:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_00
+    .byte 4
+    .word Aob_Pupuri_Parts_01
+    .byte 8
+    .word Aob_Pupuri_Parts_08
+    .byte 3
+    .word Aob_Pupuri_Parts_03
+    .byte 4
+    .word Aob_Pupuri_Parts_04
+    .byte 50
+    .word Aob_Pupuri_Parts_0f
+    .byte 2
+    .word Aob_Pupuri_Parts_07
+    .byte 4
+    .word Aob_Pupuri_Parts_10
+    .byte 0xF8
+    .word 0x200B299
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani00
+
+    .global Aob_Pupuri_Parts_Ani04
+Aob_Pupuri_Parts_Ani04:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_00
+    .byte 3
+    .word Aob_Pupuri_Parts_01
+    .byte 8
+    .word Aob_Pupuri_Parts_08
+    .byte 2
+    .word Aob_Pupuri_Parts_03
+    .byte 3
+    .word Aob_Pupuri_Parts_04
+    .byte 12
+    .word Aob_Pupuri_Parts_0f
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_0f
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_06
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 2
+    .word Aob_Pupuri_Parts_07
+    .byte 4
+    .word Aob_Pupuri_Parts_10
+    .byte 0xF8
+    .word 0x200B299
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani00
+
+    .global Aob_Pupuri_Parts_Ani05
+Aob_Pupuri_Parts_Ani05:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_00
+    .byte 4
+    .word Aob_Pupuri_Parts_01
+    .byte 8
+    .word Aob_Pupuri_Parts_08
+    .byte 3
+    .word Aob_Pupuri_Parts_03
+    .byte 4
+    .word Aob_Pupuri_Parts_04
+    .byte 12
+    .word Aob_Pupuri_Parts_0f
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_0f
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_06
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 2
+    .word Aob_Pupuri_Parts_07
+    .byte 4
+    .word Aob_Pupuri_Parts_10
+    .byte 0xF8
+    .word 0x200B299
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani00
+
+    .global Aob_Pupuri_Parts_Ani06
+Aob_Pupuri_Parts_Ani06:
+    .byte 0xFB
+    .word 0
+    .byte 1
+    .word Aob_Pupuri_Parts_00
+    .byte 4
+    .word Aob_Pupuri_Parts_01
+    .byte 8
+    .word Aob_Pupuri_Parts_08
+    .byte 3
+    .word Aob_Pupuri_Parts_03
+    .byte 4
+    .word Aob_Pupuri_Parts_04
+    .byte 12
+    .word Aob_Pupuri_Parts_0f
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_02
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_06
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 3
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 3
+    .word Aob_Pupuri_Parts_05
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 3
+    .word Aob_Pupuri_Parts_05
+    .byte 3
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 3
+    .word Aob_Pupuri_Parts_05
+    .byte 2
+    .word Aob_Pupuri_Parts_07
+    .byte 4
+    .word Aob_Pupuri_Parts_10
+    .byte 0xF8
+    .word 0x200B299
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani00
+
+    .global Aob_Pupuri_Parts_Ani07
+Aob_Pupuri_Parts_Ani07:
+    .byte 0xFB
+    .word 0
+    .byte 3
+    .word Aob_Pupuri_Parts_03
+    .byte 4
+    .word Aob_Pupuri_Parts_04
+    .byte 12
+    .word Aob_Pupuri_Parts_0f
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_02
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 8
+    .word Aob_Pupuri_Parts_06
+    .byte 4
+    .word Aob_Pupuri_Parts_05
+    .byte 10
+    .word Aob_Pupuri_Parts_06
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 8
+    .word Aob_Pupuri_Parts_0a
+    .byte 10
+    .word Aob_Pupuri_Parts_09
+    .byte 3
+    .word Aob_Pupuri_Parts_0a
+Aob_Pupuri_Parts_Ani07_1:
+    .byte 16
+    .word Aob_Pupuri_Parts_09
+    .byte 16
+    .word Aob_Pupuri_Parts_0a
+    .byte 0xF5
+    .word Aob_Pupuri_Parts_Ani07_1
