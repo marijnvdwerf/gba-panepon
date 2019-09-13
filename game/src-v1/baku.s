46bf90ab75548dafa9ad703ef5198af9cd9ba5c7
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2019-09-13 03:29:37 +0200

    fix v1 build

---
+++ game/src-v1/baku.s
@@ -0,0 +1,797 @@
+    .global Parts_20014EC
+Parts_20014EC:
+    .2byte 2
+    .2byte 0x0002, 0x0002, 0x8602
+    .2byte 0x00F6, 0x01F6, 0x8602
+
+    .align 2
+    .global Parts_20014FC
+Parts_20014FC:
+    .2byte 2
+    .2byte 0x0003, 0x01F5, 0x8602
+    .2byte 0x00F5, 0x0003, 0x8602
+
+    .align 2
+    .global Parts_200150C
+Parts_200150C:
+    .2byte 2
+    .2byte 0x0004, 0x0004, 0x8602
+    .2byte 0x00F4, 0x01F4, 0x8602
+
+    .align 2
+    .global Parts_200151C
+Parts_200151C:
+    .2byte 2
+    .2byte 0x0004, 0x01F4, 0x8602
+    .2byte 0x00F4, 0x0004, 0x8602
+
+    .align 2
+    .global Parts_200152C
+Parts_200152C:
+    .2byte 2
+    .2byte 0x0005, 0x0005, 0x8602
+    .2byte 0x00F3, 0x01F3, 0x8602
+
+    .align 2
+    .global Parts_200153C
+Parts_200153C:
+    .2byte 2
+    .2byte 0x0006, 0x01F2, 0x8602
+    .2byte 0x00F2, 0x0006, 0x8602
+
+    .align 2
+    .global Parts_200154C
+Parts_200154C:
+    .2byte 2
+    .2byte 0x0006, 0x0006, 0x8603
+    .2byte 0x00F2, 0x01F2, 0x8603
+
+    .align 2
+    .global Parts_200155C
+Parts_200155C:
+    .2byte 2
+    .2byte 0x0007, 0x01F1, 0x8603
+    .2byte 0x00F1, 0x0007, 0x8603
+
+    .align 2
+    .global Parts_200156C
+Parts_200156C:
+    .2byte 2
+    .2byte 0x0008, 0x0008, 0x8603
+    .2byte 0x00F0, 0x01F0, 0x8603
+
+    .align 2
+    .global Parts_200157C
+Parts_200157C:
+    .2byte 2
+    .2byte 0x0008, 0x01F0, 0x8603
+    .2byte 0x00F0, 0x0008, 0x8603
+
+    .align 2
+    .global Parts_200158C
+Parts_200158C:
+    .2byte 2
+    .2byte 0x0009, 0x0009, 0x8603
+    .2byte 0x00EF, 0x01EF, 0x8603
+
+    .align 2
+    .global Parts_200159C
+Parts_200159C:
+    .2byte 2
+    .2byte 0x0009, 0x01EF, 0x8603
+    .2byte 0x00EF, 0x0009, 0x8603
+
+    .align 2
+    .global Parts_20015AC
+Parts_20015AC:
+    .2byte 2
+    .2byte 0x000A, 0x000A, 0x8604
+    .2byte 0x00EE, 0x01EE, 0x8604
+
+    .align 2
+    .global Parts_20015BC
+Parts_20015BC:
+    .2byte 2
+    .2byte 0x000A, 0x01EE, 0x8604
+    .2byte 0x00EE, 0x000A, 0x8604
+
+    .align 2
+    .global Parts_20015CC
+Parts_20015CC:
+    .2byte 2
+    .2byte 0x000A, 0x000A, 0x8605
+    .2byte 0x00EE, 0x01EE, 0x8605
+
+    .align 2
+    .global Parts_20015DC
+Parts_20015DC:
+    .2byte 2
+    .2byte 0x000B, 0x01ED, 0x8605
+    .2byte 0x00ED, 0x000B, 0x8605
+
+    .align 2
+    .global Parts_20015EC
+Parts_20015EC:
+    .2byte 2
+    .2byte 0x000B, 0x000B, 0x8606
+    .2byte 0x00ED, 0x01ED, 0x8606
+
+    .align 2
+    .global Parts_20015FC
+Parts_20015FC:
+    .2byte 2
+    .2byte 0x000B, 0x01ED, 0x8606
+    .2byte 0x00ED, 0x000B, 0x8606
+
+    .align 2
+    .global Parts_200160C
+Parts_200160C:
+    .2byte 2
+    .2byte 0x000B, 0x000B, 0x8607
+    .2byte 0x00ED, 0x01ED, 0x8607
+
+    .align 2
+    .global Parts_200161C
+Parts_200161C:
+    .2byte 2
+    .2byte 0x000C, 0x01EC, 0x8607
+    .2byte 0x00EC, 0x000C, 0x8607
+
+    .align 2
+    .global Parts_200162C
+Parts_200162C:
+    .2byte 2
+    .2byte 0x000C, 0x000C, 0x8608
+    .2byte 0x00EC, 0x01EC, 0x8608
+
+    .align 2
+    .global Parts_200163C
+Parts_200163C:
+    .2byte 2
+    .2byte 0x000C, 0x01EC, 0x8608
+    .2byte 0x00EC, 0x000C, 0x8608
+
+    .align 2
+    .global Parts_200164C
+Parts_200164C:
+    .2byte 2
+    .2byte 0x000C, 0x000C, 0x8609
+    .2byte 0x00EC, 0x01EC, 0x8609
+
+    .align 2
+    .global Parts_200165C
+Parts_200165C:
+    .2byte 2
+    .2byte 0x0003, 0x0003, 0x8602
+    .2byte 0x00F5, 0x01F5, 0x8602
+
+    .align 2
+    .global Parts_200166C
+Parts_200166C:
+    .2byte 2
+    .2byte 0x0004, 0x01F4, 0x8602
+    .2byte 0x00F4, 0x0004, 0x8602
+
+    .align 2
+    .global Parts_200167C
+Parts_200167C:
+    .2byte 2
+    .2byte 0x0005, 0x0005, 0x8602
+    .2byte 0x00F3, 0x01F3, 0x8602
+
+    .align 2
+    .global Parts_200168C
+Parts_200168C:
+    .2byte 2
+    .2byte 0x0006, 0x01F2, 0x8602
+    .2byte 0x00F2, 0x0006, 0x8602
+
+    .align 2
+    .global Parts_200169C
+Parts_200169C:
+    .2byte 2
+    .2byte 0x0007, 0x0007, 0x8602
+    .2byte 0x00F1, 0x01F1, 0x8602
+
+    .align 2
+    .global Parts_20016AC
+Parts_20016AC:
+    .2byte 2
+    .2byte 0x0007, 0x01F1, 0x8602
+    .2byte 0x00F1, 0x0007, 0x8602
+
+    .align 2
+    .global Parts_20016BC
+Parts_20016BC:
+    .2byte 2
+    .2byte 0x0008, 0x0008, 0x8603
+    .2byte 0x00F0, 0x01F0, 0x8603
+
+    .align 2
+    .global Parts_20016CC
+Parts_20016CC:
+    .2byte 2
+    .2byte 0x0009, 0x01EF, 0x8603
+    .2byte 0x00EF, 0x0009, 0x8603
+
+    .align 2
+    .global Parts_20016DC
+Parts_20016DC:
+    .2byte 2
+    .2byte 0x000A, 0x000A, 0x8603
+    .2byte 0x00EE, 0x01EE, 0x8603
+
+    .align 2
+    .global Parts_20016EC
+Parts_20016EC:
+    .2byte 2
+    .2byte 0x000A, 0x01EE, 0x8603
+    .2byte 0x00EE, 0x000A, 0x8603
+
+    .align 2
+    .global Parts_20016FC
+Parts_20016FC:
+    .2byte 2
+    .2byte 0x000B, 0x000B, 0x8603
+    .2byte 0x00ED, 0x01ED, 0x8603
+
+    .align 2
+    .global Parts_200170C
+Parts_200170C:
+    .2byte 2
+    .2byte 0x000C, 0x01EC, 0x8603
+    .2byte 0x00EC, 0x000C, 0x8603
+
+    .align 2
+    .global Parts_200171C
+Parts_200171C:
+    .2byte 2
+    .2byte 0x000C, 0x000C, 0x8604
+    .2byte 0x00EC, 0x01EC, 0x8604
+
+    .align 2
+    .global Parts_200172C
+Parts_200172C:
+    .2byte 2
+    .2byte 0x000D, 0x01EB, 0x8604
+    .2byte 0x00EB, 0x000D, 0x8604
+
+    .align 2
+    .global Parts_200173C
+Parts_200173C:
+    .2byte 2
+    .2byte 0x000D, 0x000D, 0x8605
+    .2byte 0x00EB, 0x01EB, 0x8605
+
+    .align 2
+    .global Parts_200174C
+Parts_200174C:
+    .2byte 2
+    .2byte 0x000E, 0x01EA, 0x8605
+    .2byte 0x00EA, 0x000E, 0x8605
+
+    .align 2
+    .global Parts_200175C
+Parts_200175C:
+    .2byte 2
+    .2byte 0x000E, 0x000E, 0x8606
+    .2byte 0x00EA, 0x01EA, 0x8606
+
+    .align 2
+    .global Parts_200176C
+Parts_200176C:
+    .2byte 2
+    .2byte 0x000E, 0x01EA, 0x8606
+    .2byte 0x00EA, 0x000E, 0x8606
+
+    .align 2
+    .global Parts_200177C
+Parts_200177C:
+    .2byte 2
+    .2byte 0x000F, 0x000F, 0x8607
+    .2byte 0x00E9, 0x01E9, 0x8607
+
+    .align 2
+    .global Parts_200178C
+Parts_200178C:
+    .2byte 2
+    .2byte 0x000F, 0x01E9, 0x8607
+    .2byte 0x00E9, 0x000F, 0x8607
+
+    .align 2
+    .global Parts_200179C
+Parts_200179C:
+    .2byte 2
+    .2byte 0x000F, 0x000F, 0x8608
+    .2byte 0x00E9, 0x01E9, 0x8608
+
+    .align 2
+    .global Parts_20017AC
+Parts_20017AC:
+    .2byte 2
+    .2byte 0x000F, 0x01E9, 0x8608
+    .2byte 0x00E9, 0x000F, 0x8608
+
+    .align 2
+    .global Parts_20017BC
+Parts_20017BC:
+    .2byte 2
+    .2byte 0x000F, 0x000F, 0x8609
+    .2byte 0x00E9, 0x01E9, 0x8609
+
+    .align 2
+    .global Parts_20017CC
+Parts_20017CC:
+    .2byte 3
+    .2byte 0x0001, 0x0005, 0x8602
+    .2byte 0x0001, 0x01F3, 0x8602
+    .2byte 0x00F2, 0x01FC, 0x8602
+
+    .global Parts_20017E0
+Parts_20017E0:
+    .2byte 3
+    .2byte 0x0008, 0x01FC, 0x8602
+    .2byte 0x00F6, 0x01F2, 0x8602
+    .2byte 0x00F6, 0x0006, 0x8602
+
+    .global Parts_20017F4
+Parts_20017F4:
+    .2byte 3
+    .2byte 0x0003, 0x0008, 0x8602
+    .2byte 0x0003, 0x01F0, 0x8602
+    .2byte 0x00EE, 0x01FC, 0x8602
+
+    .global Parts_2001808
+Parts_2001808:
+    .2byte 3
+    .2byte 0x000B, 0x01FC, 0x8602
+    .2byte 0x00F5, 0x01EF, 0x8602
+    .2byte 0x00F5, 0x0009, 0x8602
+
+    .global Parts_200181C
+Parts_200181C:
+    .2byte 3
+    .2byte 0x0004, 0x000B, 0x8602
+    .2byte 0x0004, 0x01ED, 0x8602
+    .2byte 0x00EB, 0x01FC, 0x8602
+
+    .global Parts_2001830
+Parts_2001830:
+    .2byte 3
+    .2byte 0x000E, 0x01FC, 0x8602
+    .2byte 0x00F3, 0x01EC, 0x8602
+    .2byte 0x00F3, 0x000C, 0x8602
+
+    .global Parts_2001844
+Parts_2001844:
+    .2byte 3
+    .2byte 0x0006, 0x000D, 0x8603
+    .2byte 0x0006, 0x01EB, 0x8603
+    .2byte 0x00E8, 0x01FC, 0x8603
+
+    .global Parts_2001858
+Parts_2001858:
+    .2byte 3
+    .2byte 0x0011, 0x01FC, 0x8603
+    .2byte 0x00F2, 0x01EA, 0x8603
+    .2byte 0x00F2, 0x000E, 0x8603
+
+    .global Parts_200186C
+Parts_200186C:
+    .2byte 3
+    .2byte 0x0007, 0x000F, 0x8603
+    .2byte 0x0007, 0x01E9, 0x8603
+    .2byte 0x00E6, 0x01FC, 0x8603
+
+    .global Parts_2001880
+Parts_2001880:
+    .2byte 3
+    .2byte 0x0013, 0x01FC, 0x8603
+    .2byte 0x00F1, 0x01E8, 0x8603
+    .2byte 0x00F1, 0x0010, 0x8603
+
+    .global Parts_2001894
+Parts_2001894:
+    .2byte 3
+    .2byte 0x0008, 0x0011, 0x8603
+    .2byte 0x0008, 0x01E7, 0x8603
+    .2byte 0x00E4, 0x01FC, 0x8603
+
+    .global Parts_20018A8
+Parts_20018A8:
+    .2byte 3
+    .2byte 0x0015, 0x01FC, 0x8603
+    .2byte 0x00F0, 0x01E6, 0x8603
+    .2byte 0x00F0, 0x0012, 0x8603
+
+    .global Parts_20018BC
+Parts_20018BC:
+    .2byte 3
+    .2byte 0x0009, 0x0012, 0x8604
+    .2byte 0x0009, 0x01E6, 0x8604
+    .2byte 0x00E2, 0x01FC, 0x8604
+
+    .global Parts_20018D0
+Parts_20018D0:
+    .2byte 3
+    .2byte 0x0017, 0x01FC, 0x8604
+    .2byte 0x00EF, 0x01E5, 0x8604
+    .2byte 0x00EF, 0x0013, 0x8604
+
+    .global Parts_20018E4
+Parts_20018E4:
+    .2byte 3
+    .2byte 0x000A, 0x0014, 0x8605
+    .2byte 0x000A, 0x01E4, 0x8605
+    .2byte 0x00E0, 0x01FC, 0x8605
+
+    .global Parts_20018F8
+Parts_20018F8:
+    .2byte 3
+    .2byte 0x0018, 0x01FC, 0x8605
+    .2byte 0x00EE, 0x01E4, 0x8605
+    .2byte 0x00EE, 0x0014, 0x8605
+
+    .global Parts_200190C
+Parts_200190C:
+    .2byte 3
+    .2byte 0x000A, 0x0015, 0x8606
+    .2byte 0x000A, 0x01E3, 0x8606
+    .2byte 0x00DF, 0x01FC, 0x8606
+
+    .global Parts_2001920
+Parts_2001920:
+    .2byte 3
+    .2byte 0x0019, 0x01FC, 0x8606
+    .2byte 0x00EE, 0x01E3, 0x8606
+    .2byte 0x00EE, 0x0015, 0x8606
+
+    .global Parts_2001934
+Parts_2001934:
+    .2byte 3
+    .2byte 0x000B, 0x0016, 0x8607
+    .2byte 0x000B, 0x01E2, 0x8607
+    .2byte 0x00DE, 0x01FC, 0x8607
+
+    .global Parts_2001948
+Parts_2001948:
+    .2byte 3
+    .2byte 0x001A, 0x01FC, 0x8607
+    .2byte 0x00ED, 0x01E2, 0x8607
+    .2byte 0x00ED, 0x0016, 0x8607
+
+    .global Parts_200195C
+Parts_200195C:
+    .2byte 3
+    .2byte 0x000B, 0x0016, 0x8608
+    .2byte 0x000B, 0x01E2, 0x8608
+    .2byte 0x00DE, 0x01FC, 0x8608
+
+    .global Parts_2001970
+Parts_2001970:
+    .2byte 3
+    .2byte 0x001B, 0x01FC, 0x8608
+    .2byte 0x00ED, 0x01E1, 0x8608
+    .2byte 0x00ED, 0x0017, 0x8608
+
+    .global Parts_2001984
+Parts_2001984:
+    .2byte 3
+    .2byte 0x000B, 0x0017, 0x8609
+    .2byte 0x000B, 0x01E1, 0x8609
+    .2byte 0x00DD, 0x01FC, 0x8609
+
+    .global Parts_2001998
+Parts_2001998:
+    .2byte 4
+    .2byte 0x00FC, 0x0008, 0x8602
+    .2byte 0x0007, 0x01FC, 0x8602
+    .2byte 0x00FC, 0x01F0, 0x8602
+    .2byte 0x00F1, 0x01FC, 0x8602
+
+    .align 2
+    .global Parts_20019B4
+Parts_20019B4:
+    .2byte 4
+    .2byte 0x0005, 0x0005, 0x8602
+    .2byte 0x0005, 0x01F3, 0x8602
+    .2byte 0x00F3, 0x01F3, 0x8602
+    .2byte 0x00F3, 0x0005, 0x8602
+
+    .align 2
+    .global Parts_20019D0
+Parts_20019D0:
+    .2byte 4
+    .2byte 0x00FC, 0x000B, 0x8602
+    .2byte 0x000B, 0x01FC, 0x8602
+    .2byte 0x00FC, 0x01ED, 0x8602
+    .2byte 0x00ED, 0x01FC, 0x8602
+
+    .align 2
+    .global Parts_20019EC
+Parts_20019EC:
+    .2byte 4
+    .2byte 0x0008, 0x0008, 0x8602
+    .2byte 0x0008, 0x01F0, 0x8602
+    .2byte 0x00F0, 0x01F0, 0x8602
+    .2byte 0x00F0, 0x0008, 0x8602
+
+    .align 2
+    .global Parts_2001A08
+Parts_2001A08:
+    .2byte 4
+    .2byte 0x00FC, 0x000F, 0x8602
+    .2byte 0x000F, 0x01FC, 0x8602
+    .2byte 0x00FC, 0x01E9, 0x8602
+    .2byte 0x00E9, 0x01FC, 0x8602
+
+    .align 2
+    .global Parts_2001A24
+Parts_2001A24:
+    .2byte 4
+    .2byte 0x000A, 0x000A, 0x8602
+    .2byte 0x000A, 0x01EE, 0x8602
+    .2byte 0x00EE, 0x01EE, 0x8602
+    .2byte 0x00EE, 0x000A, 0x8602
+
+    .align 2
+    .global Parts_2001A40
+Parts_2001A40:
+    .2byte 4
+    .2byte 0x00FC, 0x0012, 0x8603
+    .2byte 0x0012, 0x01FC, 0x8603
+    .2byte 0x00FC, 0x01E6, 0x8603
+    .2byte 0x00E6, 0x01FC, 0x8603
+
+    .align 2
+    .global Parts_2001A5C
+Parts_2001A5C:
+    .2byte 4
+    .2byte 0x000C, 0x000C, 0x8603
+    .2byte 0x000C, 0x01EC, 0x8603
+    .2byte 0x00EC, 0x01EC, 0x8603
+    .2byte 0x00EC, 0x000C, 0x8603
+
+    .align 2
+    .global Parts_2001A78
+Parts_2001A78:
+    .2byte 4
+    .2byte 0x00FC, 0x0014, 0x8603
+    .2byte 0x0014, 0x01FC, 0x8603
+    .2byte 0x00FC, 0x01E4, 0x8603
+    .2byte 0x00E4, 0x01FC, 0x8603
+
+    .align 2
+    .global Parts_2001A94
+Parts_2001A94:
+    .2byte 4
+    .2byte 0x000E, 0x000E, 0x8603
+    .2byte 0x000E, 0x01EA, 0x8603
+    .2byte 0x00EA, 0x01EA, 0x8603
+    .2byte 0x00EA, 0x000E, 0x8603
+
+    .align 2
+    .global Parts_2001AB0
+Parts_2001AB0:
+    .2byte 4
+    .2byte 0x00FC, 0x0017, 0x8603
+    .2byte 0x0017, 0x01FC, 0x8603
+    .2byte 0x00FC, 0x01E1, 0x8603
+    .2byte 0x00E1, 0x01FC, 0x8603
+
+    .align 2
+    .global Parts_2001ACC
+Parts_2001ACC:
+    .2byte 4
+    .2byte 0x0010, 0x0010, 0x8603
+    .2byte 0x0010, 0x01E8, 0x8603
+    .2byte 0x00E8, 0x01E8, 0x8603
+    .2byte 0x00E8, 0x0010, 0x8603
+
+    .align 2
+    .global Parts_2001AE8
+Parts_2001AE8:
+    .2byte 4
+    .2byte 0x00FC, 0x0019, 0x8604
+    .2byte 0x0019, 0x01FC, 0x8604
+    .2byte 0x00FC, 0x01DF, 0x8604
+    .2byte 0x00DF, 0x01FC, 0x8604
+
+    .align 2
+    .global Parts_2001B04
+Parts_2001B04:
+    .2byte 4
+    .2byte 0x0011, 0x0011, 0x8604
+    .2byte 0x0011, 0x01E7, 0x8604
+    .2byte 0x00E7, 0x01E7, 0x8604
+    .2byte 0x00E7, 0x0011, 0x8604
+
+    .align 2
+    .global Parts_2001B20
+Parts_2001B20:
+    .2byte 4
+    .2byte 0x00FC, 0x001B, 0x8605
+    .2byte 0x001B, 0x01FC, 0x8605
+    .2byte 0x00FC, 0x01DD, 0x8605
+    .2byte 0x00DD, 0x01FC, 0x8605
+
+    .align 2
+    .global Parts_2001B3C
+Parts_2001B3C:
+    .2byte 4
+    .2byte 0x0012, 0x0012, 0x8605
+    .2byte 0x0012, 0x01E6, 0x8605
+    .2byte 0x00E6, 0x01E6, 0x8605
+    .2byte 0x00E6, 0x0012, 0x8605
+
+    .align 2
+    .global Parts_2001B58
+Parts_2001B58:
+    .2byte 4
+    .2byte 0x00FC, 0x001C, 0x8606
+    .2byte 0x001C, 0x01FC, 0x8606
+    .2byte 0x00FC, 0x01DC, 0x8606
+    .2byte 0x00DC, 0x01FC, 0x8606
+
+    .align 2
+    .global Parts_2001B74
+Parts_2001B74:
+    .2byte 4
+    .2byte 0x0013, 0x0013, 0x8606
+    .2byte 0x0013, 0x01E5, 0x8606
+    .2byte 0x00E5, 0x01E5, 0x8606
+    .2byte 0x00E5, 0x0013, 0x8606
+
+    .align 2
+    .global Parts_2001B90
+Parts_2001B90:
+    .2byte 4
+    .2byte 0x00FC, 0x001D, 0x8607
+    .2byte 0x001D, 0x01FC, 0x8607
+    .2byte 0x00FC, 0x01DB, 0x8607
+    .2byte 0x00DB, 0x01FC, 0x8607
+
+    .align 2
+    .global Parts_2001BAC
+Parts_2001BAC:
+    .2byte 4
+    .2byte 0x0014, 0x0014, 0x8607
+    .2byte 0x0014, 0x01E4, 0x8607
+    .2byte 0x00E4, 0x01E4, 0x8607
+    .2byte 0x00E4, 0x0014, 0x8607
+
+    .align 2
+    .global Parts_2001BC8
+Parts_2001BC8:
+    .2byte 4
+    .2byte 0x00FC, 0x001E, 0x8608
+    .2byte 0x001E, 0x01FC, 0x8608
+    .2byte 0x00FC, 0x01DA, 0x8608
+    .2byte 0x00DA, 0x01FC, 0x8608
+
+    .align 2
+    .global Parts_2001BE4
+Parts_2001BE4:
+    .2byte 4
+    .2byte 0x0014, 0x0014, 0x8608
+    .2byte 0x0014, 0x01E4, 0x8608
+    .2byte 0x00E4, 0x01E4, 0x8608
+    .2byte 0x00E4, 0x0014, 0x8608
+
+    .align 2
+    .global Parts_2001C00
+Parts_2001C00:
+    .2byte 4
+    .2byte 0x00FC, 0x001E, 0x8609
+    .2byte 0x001E, 0x01FC, 0x8609
+    .2byte 0x00FC, 0x01DA, 0x8609
+    .2byte 0x00DA, 0x01FC, 0x8609
+
+    .align 2
+    .global Aob_Panel_Baku_Ani00
+Aob_Panel_Baku_Ani00:
+    .4byte Parts_20014EC
+    .4byte Parts_20014FC
+    .4byte Parts_200150C
+    .4byte Parts_200151C
+    .4byte Parts_200152C
+    .4byte Parts_200153C
+    .4byte Parts_200154C
+    .4byte Parts_200155C
+    .4byte Parts_200156C
+    .4byte Parts_200157C
+    .4byte Parts_200158C
+    .4byte Parts_200159C
+    .4byte Parts_20015AC
+    .4byte Parts_20015BC
+    .4byte Parts_20015CC
+    .4byte Parts_20015DC
+    .4byte Parts_20015EC
+    .4byte Parts_20015FC
+    .4byte Parts_200160C
+    .4byte Parts_200161C
+    .4byte Parts_200162C
+    .4byte Parts_200163C
+    .4byte Parts_200164C
+    .4byte 0
+
+    .global Aob_Panel_Baku_Ani01
+Aob_Panel_Baku_Ani01:
+    .4byte Parts_200165C
+    .4byte Parts_200166C
+    .4byte Parts_200167C
+    .4byte Parts_200168C
+    .4byte Parts_200169C
+    .4byte Parts_20016AC
+    .4byte Parts_20016BC
+    .4byte Parts_20016CC
+    .4byte Parts_20016DC
+    .4byte Parts_20016EC
+    .4byte Parts_20016FC
+    .4byte Parts_200170C
+    .4byte Parts_200171C
+    .4byte Parts_200172C
+    .4byte Parts_200173C
+    .4byte Parts_200174C
+    .4byte Parts_200175C
+    .4byte Parts_200176C
+    .4byte Parts_200177C
+    .4byte Parts_200178C
+    .4byte Parts_200179C
+    .4byte Parts_20017AC
+    .4byte Parts_20017BC
+    .4byte 0
+
+    .global Aob_Panel_Baku_Ani02
+Aob_Panel_Baku_Ani02:
+    .4byte Parts_20017CC
+    .4byte Parts_20017E0
+    .4byte Parts_20017F4
+    .4byte Parts_2001808
+    .4byte Parts_200181C
+    .4byte Parts_2001830
+    .4byte Parts_2001844
+    .4byte Parts_2001858
+    .4byte Parts_200186C
+    .4byte Parts_2001880
+    .4byte Parts_2001894
+    .4byte Parts_20018A8
+    .4byte Parts_20018BC
+    .4byte Parts_20018D0
+    .4byte Parts_20018E4
+    .4byte Parts_20018F8
+    .4byte Parts_200190C
+    .4byte Parts_2001920
+    .4byte Parts_2001934
+    .4byte Parts_2001948
+    .4byte Parts_200195C
+    .4byte Parts_2001970
+    .4byte Parts_2001984
+    .4byte 0
+
+    .global Aob_Panel_Baku_Ani03
+Aob_Panel_Baku_Ani03:
+    .4byte Parts_2001998
+    .4byte Parts_20019B4
+    .4byte Parts_20019D0
+    .4byte Parts_20019EC
+    .4byte Parts_2001A08
+    .4byte Parts_2001A24
+    .4byte Parts_2001A40
+    .4byte Parts_2001A5C
+    .4byte Parts_2001A78
+    .4byte Parts_2001A94
+    .4byte Parts_2001AB0
+    .4byte Parts_2001ACC
+    .4byte Parts_2001AE8
+    .4byte Parts_2001B04
+    .4byte Parts_2001B20
+    .4byte Parts_2001B3C
+    .4byte Parts_2001B58
+    .4byte Parts_2001B74
+    .4byte Parts_2001B90
+    .4byte Parts_2001BAC
+    .4byte Parts_2001BC8
+    .4byte Parts_2001BE4
+    .4byte Parts_2001C00
+    .4byte 0
